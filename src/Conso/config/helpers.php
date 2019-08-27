17d75ac3e1710940d2daecc60fd39946e98740d5
lotfio <contact@lotfio.net>
2019-08-27 22:29:58 +0100

    adding read namespace of file

--- src/Conso/config/helpers.php
+++ src/Conso/config/helpers.php
@@ -45,3 +45,22 @@ if (!function_exists('isTest')) {
         return (strpos($file[count($file) - 1], 'phpunit') !== false) ? true : false;
     }
 }
+
+if(!function_exists('extractNamespace'))
+{
+    function extractNamespace($file) {
+        $ns = NULL;
+        $handle = fopen($file, "r");
+        if ($handle) {
+            while (($line = fgets($handle)) !== false) {
+                if (strpos($line, 'namespace') === 0) {
+                    $parts = explode(' ', $line);
+                    $ns = rtrim(trim($parts[1]), ';');
+                    break;
+                }
+            }
+            fclose($handle);
+        }
+        return $ns;
+    }
+}
